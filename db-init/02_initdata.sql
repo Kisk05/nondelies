81b5d13678e16499d5ca5f348ffb062b8b91b053
正田 陸 <kd1340354@kobedenshi078.onmicrosoft.com>
2025-11-19 10:06:04 +0900

    DB初期データ

---
+++ db-init/02_initdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO super(sup_name) VALUES("マルハチ");
+INSERT INTO super(sup_name) VALUES("マルアイ");
+INSERT INTO super(sup_name) VALUES("関西スーパー");
+INSERT INTO super(sup_name) VALUES("マックスバリュ");
+INSERT INTO super(sup_name) VALUES("業務スーパー");
+INSERT INTO super(sup_name) VALUES("コープ");
